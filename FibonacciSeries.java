89b9049708c581b5b6214a7f937419d3cfb09c69
vincent f. liu <fengxliu@gmail.com>
2014-12-30 02:27:25 -0500

    Update FibonacciSeries.java

--- FibonacciSeries.java
+++ FibonacciSeries.java
@@ -1,29 +1,32 @@
-public class FibonacciSeries {
-	
-	/** recursion */ 
-	public static int F(int n){
-		if(n == 0) return 0; 
-		if(n == 1) return 1; 
-		else return F(n - 1) + F(n - 2); 
-	}
-	
-	/** dynamic programming: O(n) runtime */ 
-	public static int Fs(int n)
-	{
-		int[] array = {0, 1, 0}; 
-		for (int i = 2; i < n + 1; i ++ ){
-			array[i%3] = array[(i-1)%3] + array[(i-2)%3]; 
+import java.io.*; 
+
+public class Main {
+
+	/**  O(n) runtime */
+	public static int Fs(int n) {
+		int[] array = { 0, 1, 0 };
+		for (int i = 2; i < n + 1; i++) {
+			array[i % 3] = array[(i - 1) % 3] + array[(i - 2) % 3];
 		}
-		return array[n%3]; 
+		return array[n % 3];
 	}
 
 	public static void main(String[] args) {
-	  int n = 40; 
-		for(int i = 0; i < n; i++){
-			System.out.printf("%d ", Fs(i));
-			if((i + 1)%10 == 0) System.out.println();
-		}
 		
-	}
+		File file = new File("test_cases.txt");
+		try {
+			BufferedReader in = new BufferedReader(new FileReader(file));
+			String line;
+			while ((line = in.readLine()) != null) 
+			{
+				line = line.trim();
+				int i = Integer.parseInt(line);
+		        	System.out.println(Fs(i));
+			}
 
+		} catch (IOException e) {
+			System.out.println("File Read Error: " + e.getMessage());
+		}
+
+	}
 }
